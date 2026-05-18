694d15c81be32f8eb73e45e9d76268e111cbd703
stavioagency <stavio.agency@gmail.com>
2026-05-18 22:52:47 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="ar" dir="rtl">
       <Head>
         <meta name="theme-color" content="#0a0a0c" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </Head>
       <body>
         <Main />
