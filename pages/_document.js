abfdd6ceb67c533511767823a7d1854c55757cc9
stavioagency <stavio.agency@gmail.com>
2026-05-21 04:54:36 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,8 @@ export default function Document() {
     <Html lang="ar" dir="rtl">
       <Head>
         <meta name="theme-color" content="#0a0a0c" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
       </Head>
       <body>
         <Main />
